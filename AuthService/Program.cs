66ac779db28ab2aae2afb7f65c91475eb3ec89c6
DARSHAN DEVAMANE <darshan.vdevamane@cognizant.com>
2026-06-05 11:20:18 +0530

    Add Dockerfiles for all microservices and Angular frontend with docker-compose

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -123,7 +123,7 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth Servic
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 
     // Seed default admin if no users exist
     if (!db.Users.Any())
